bcddc4dd9c56c517dafa3132b6d407e2ef505b91
vlavirus <androando777@gmail.com>
2020-02-25 18:51:10 +0300

    feat: add lgorithm

--- src/index.js
+++ src/index.js
@@ -1,12 +1,57 @@
 
+function check_array(array) {
+  if ( Array.isArray(array) == false || array.length == '0' ) {
+    return 0;
+  }
+}
+
 exports.min = function min (array) {
-  return 0;
+  
+  if (check_array(array) == 0) {
+    return 0;
+  }
+
+  let min = array[0];
+  let size = array.length;
+
+    for ( let count = 1; count <= size; count++ ) {
+      if( array[count] < min ) {
+        min = array[count];
+      }
+    }
+  return min;
 }
 
 exports.max = function max (array) {
-  return 0;
+  if (check_array(array) == 0) {
+    return 0;
+  }
+
+  let max = array[0];
+  let size = array.length;
+
+    for ( let count = 1; count <= size; count++ ) {
+      if( array[count] > max ) {
+        max = array[count];
+      }
+    }
+  return max;
 }
 
 exports.avg = function avg (array) {
-  return 0;
+  if (check_array(array) == 0) {
+    return 0;
+  }
+
+  let size = array.length;
+  let array_sum = 0;
+  let array_avg;
+
+  for ( let count = 0; count < size; count++ ) {
+    array_sum += array[count];
+  }
+
+  array_avg = array_sum / size;
+
+  return array_avg;
 }
